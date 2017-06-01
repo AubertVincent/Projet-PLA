8c32a0d406c1897c180bb6f8cfc060334295afce
Henry Saivet <death.note38@hotmail.fr>
2017-06-01 15:32:23 +0200

    Création des methodes de la classe entité

--- src/entite/Entite.java
+++ src/entite/Entite.java
@@ -2,4 +2,27 @@ package entite;
 
 public class Entite {
 
+	private int x;
+	private int y;
+		
+	public void Position(int x, int y){
+		this.setX(x);
+		this.setY(y);
+	}
+	
+	public void setX(int x){
+		this.x = x;
+	}
+	
+	public void setY(int y){
+		this.y = y;
+	}
+	
+	public int getX(){
+		return this.x;
+	}
+	
+	public int getY(){
+		return this.y;
+	}
 }
--- src/operateur/Mouvement.java
+++ src/operateur/Mouvement.java
@@ -2,4 +2,6 @@ package operateur;
 
 public abstract class Mouvement extends Action {
 
+	
+	public  initialiser(Joueur j);
 }
