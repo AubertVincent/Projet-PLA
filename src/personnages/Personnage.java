82cfba70c0a30085c03a05c29e95225e711f460b
Xavier <xavierdevos@outlook.fr>
2017-06-01 15:11:33 +0200

    début des stats dans personnage

--- src/personnages/Personnage.java
+++ src/personnages/Personnage.java
@@ -1,5 +1,20 @@
 package personnages;
 
 public abstract class Personnage {
-
+	
+	protected int vie;
+	protected int vision;
+	protected int attaque;
+	protected int portee;
+	protected int deplacement;
+	protected int rappel;
+		
+	public Personnage(){
+		
+	}
+	
+	public void deplacer(){
+		
+	}
+	
 }
