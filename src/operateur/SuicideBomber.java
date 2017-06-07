d7d0a70a35c41cf700f5b5af5c846e33afd01119
EnzoMolion <enzo.molion@e.ujf-grenoble.fr>
2017-06-07 09:28:00 +0200

    Conflicts resolving for merge

--- src/operateur/SuicideBomber.java
+++ src/operateur/SuicideBomber.java
@@ -2,6 +2,7 @@ package operateur;
 
 import entite.*;
 import personnages.Character;
+import personnages.Robot;
 
 public class SuicideBomber extends Attack {
 
@@ -17,17 +18,10 @@ public class SuicideBomber extends Attack {
 
 	@Override
 	public void execute(Entity e) throws GameException {
-		if (!isDoable()) {
-			throw new GameException("Cette action n'est pas réalisable");
-		}
-		if (!e.isCharacter()) {
-			throw new GameException("Cette entité n'est pas un personnage");
+		if (!isDoable(e)) {
+			throw new GameException("Cette entité n'est pas un robot");
 		} else {
-			int x = e.getX();
-			int y = e.getY();
-			Direction d;
-
-			// TODO
+			((Robot) e).suicideBomber(e);
 		}
 
 	}
