3bae19c3857ae1c4e60f91e67df0e90b076f6896
Benjam73 <besnierbenjamin73@gmail.com>
2017-06-08 13:01:03 +0200

    Add class Coordinates and class RobotList

---
+++ src/personnages/RobotList.java
@@ -0,0 +1,25 @@
+package personnages;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RobotList<robotListInterger> {
+
+	private Map<Class<?>, Map<Object, Robot>> fromClassToMap;
+
+	public RobotList() {
+		// robotListInteger = new HashMap<Integer, Robot>();
+		// robotListCoordinates = new HashMap<Coordinates, Robot>();
+		fromClassToMap = new HashMap<Class<?>, Map<Object, Robot>>();
+	}
+
+	public void add(Object obj, Robot robot) {
+		// Search if the Map for the Class of obj already existss
+		Class<?> objClass = obj.getClass();
+		if (!fromClassToMap.containsKey(objClass)) {
+			fromClassToMap.put(objClass, new HashMap<Object, Robot>());
+		}
+		fromClassToMap.get(objClass).put(obj, robot);
+	}
+
+}
