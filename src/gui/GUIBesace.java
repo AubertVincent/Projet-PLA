5e06cd5388e24209e0b0116100a90772e6ed4ae1
bastien <dearaujo.bastien@gmail.com>
2017-06-09 08:32:29 +0200

    Add besace in graphic interface

--- src/gui/GUIBesace.java
+++ src/gui/GUIBesace.java
@@ -7,19 +7,20 @@ import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.gui.TextField;
 
 public class GUIBesace {
-	private Rectangle besace;
+	private Rectangle frame;
+	
+	private Besace besace ;
 
 	protected GUIBesace(GameContainer container, int WindowWidth, int WindowHeight, int WidthRect, int HeightRect,
 			int cellWidth, int TextFieldHeight, String choices) {
 		
-		this.besace = new Rectangle(cellWidth * 7, (WindowHeight - 200) - TextFieldHeight * 4, WidthRect, HeightRect);
-		
+		this.frame = new Rectangle(cellWidth * 7, (WindowHeight - 200) - TextFieldHeight * 4, WidthRect, HeightRect);
 		
 	}
 
 	protected void render(GameContainer container, Graphics g) {
 		Color backgroundField = new Color(0f, 0f, 0f, 0.8f);
 		g.setColor(backgroundField);
-		g.fill(this.besace);
+		g.fill(this.frame);
 	}
 }
